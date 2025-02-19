23ca87a01fcd8445e4eace32107fe9a582c06e32
duchieu1999 <46242272+duchieu1999@users.noreply.github.com>
2025-02-19 19:07:07 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AccountDashboard from './components/AccountDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AccountDashboard />
+    </div>
+  );
+}
+
+export default App;
